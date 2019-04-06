6e4b7188d357188ed15670e26f89f0afbfde8b43
Víctor León <vleonbarciela@gmail.com>
2019-04-06 16:54:45 +0200

    Creo primera pantalla y aprendo a navegar entre pantallas enviando datos, para estar enfermo, suficiente.

--- BSport/BSport/BSport/MainPage.xaml.cs
+++ BSport/BSport/BSport/MainPage.xaml.cs
@@ -9,9 +9,28 @@ namespace BSport
 {
     public partial class MainPage : ContentPage
     {
+        private bool isValid = false;
         public MainPage()
         {
             InitializeComponent();
+        }
+        async void OnEnterClicked(object sender, EventArgs args)
+        {
+            if (name.Text != null && name.Text.Trim() != "" && pass.Text != null && pass.Text.Trim() != "")
+            {
+                nombre.Text = "Válido";
+                contraseña.Text = "Válida";
+                isValid = true;
+            }
+            else
+            {
+                nombre.Text = "No válido";
+                contraseña.Text = "No válida";
+            }
+            if (isValid)
+            {
+                await Navigation.PushAsync(new Menu(name.Text));
+            }
         }
     }
 }
---
+++ BSport/BSport/BSport/Menu.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BSport
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : ContentPage
+    {
+        private string nombre;
+        public Menu(string nombre)
+        {
+            InitializeComponent();
+            this.nombre = nombre;
+            test.Text = "Nombre de usuario actual " + nombre;
+        }
+        async void OnEnterClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Padel());
+        }
+    }
+}
