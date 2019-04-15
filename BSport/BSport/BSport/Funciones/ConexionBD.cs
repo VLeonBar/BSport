dc2c4f9cf791e8737d163c7e67bc159c446f03a4
Víctor León <vleonbarciela@gmail.com>
2019-04-15 23:08:46 +0200

    Intento conectar a la base de datos y me lanza excepcion, no encuentro la solución...

---
+++ BSport/BSport/BSport/Funciones/ConexionBD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace BSport.Funciones
+{
+    class ConexionBD
+    {
+        public MySqlConnection con;
+        public ConexionBD()
+        {
+
+        }
+        public bool conectaBD()
+        {
+            try
+            {
+                con = new MySqlConnection();
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("PATATA" + ex.Message);
+                return false;
+            }
+        }
+    }
+}
--- BSport/BSport/BSport/Vistas/Login.xaml.cs
+++ BSport/BSport/BSport/Vistas/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BSport.Funciones;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,17 +10,21 @@ using Xamarin.Forms.Xaml;
 
 namespace BSport.Vistas
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Login : ContentPage
-	{
-		public Login ()
-		{
-			InitializeComponent ();
-		}
-        public async void OnEnterClicked(object sender, EventArgs args) {
-            Console.WriteLine("aaaaaaa");
-            name.Text = "HOLAAA";
-            await Navigation.PushAsync(new Menu());
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
         }
-	}
+        public async void OnEnterClicked(object sender, EventArgs args)
+        {
+            ConexionBD bd = new ConexionBD();
+            if (bd.conectaBD())
+            {
+                name.Text = "CONECTADO";
+                await Navigation.PushAsync(new Menu());
+            }
+        }
+    }
 }
